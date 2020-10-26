821990cfd4441584a7c670d7813252627f787e8d
wesgriffin3 <52769190+wesgriffin3@users.noreply.github.com>
2020-10-26 12:19:14 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,9 +1,22 @@
-var arr = [];
+$(document).ready(function () {
+  $(button).on("click", function clicked() {
+    var input = document.getElementById("input").value;
+    var inputField = document.getElementById("input");
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    var form = document.getElementById("form");
+    li.appendChild(document.createTextNode(input.toUpperCase()));
+    ul.appendChild(li);
+    form.reset();
+    $(li).on("click", function () {
+      $(this).toggleClass("crossout");
+    });
+  });
+});
 
-var button = document.getElementById("btn");
-
-// on click, push the new todo to arrary
-button.addEventListener("click", function () {
-  arr.push(document.getElementById("inputText").value);
-  console.log(arr);
+$(input).keypress(function (event) {
+  if (event.which == 13) {
+    event.preventDefault();
+    $(button).click();
+  }
 });
